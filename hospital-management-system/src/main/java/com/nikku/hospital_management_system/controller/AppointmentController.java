e76917481b07b88b0b2aca755174a948c471a8c0
Nikku <kumarnikku300@gmail.com>
2026-02-09 20:14:53 +0530

    fix resources

---
+++ hospital-management-system/src/main/java/com/nikku/hospital_management_system/controller/AppointmentController.java
@@ -0,0 +1,25 @@
+package com.nikku.hospital_management_system.controller;
+
+import com.nikku.hospital_management_system.service.AppointmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/appointments")
+@RequiredArgsConstructor
+public class AppointmentController {
+    private final AppointmentService appointmentService;
+
+    @PostMapping("/book")
+    public AppointmentResponse book(
+            @RequestBody BookAppointmentRequest request) {
+
+        return appointmentService.book(request);
+    }
+
+    @GetMapping("/my")
+    public List<AppointmentResponse> myAppointments() {
+        return appointmentService.myAppointments();
+    }
+}
